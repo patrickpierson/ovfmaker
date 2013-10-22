76d876b41d019b507899293840bcb0dc85ec867b
ppiersonbt <ppierson@bericotechnologies.com>
2013-10-22 12:50:23 -0400

    0.1.0 stable

--- ovfmaker.sh
+++ ovfmaker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #This program will take in an export directory and print which vms are available for ovf-tar-gz-export
-#Then it will convert it to ovf for import into ovirt/rhev
+#Then it will convert it to ovf for import into ovirt/rhev.  Execute from export directory containing vms
 
 if [ "$1" == "" ]; then
 	#get list of exported vms
@@ -15,5 +15,6 @@ else VM_NAME=$1
 	#add images to location of image
 	VM_FIN_IMG_LOC="images/$VM_IMG_LOC"
 	#convert to tgz and label as ovf
-	tar czvf /root/$VM_NAME.ovf $VM_FIN_IMG_LOC $VM_OVF_LOC
+	echo "Saving $VM_NAME.ovf to /tmp/$VM_NAME.ovf"
+	tar czvf /tmp/$VM_NAME.ovf $VM_FIN_IMG_LOC $VM_OVF_LOC
 fi
